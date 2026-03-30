e5a164b1749d2e5b34dd0895e98ce42eeacef716
vikash <vp946203@gmail.com>
2026-03-30 23:47:46 +0530

    make routes for connections

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,38 @@ import bcrypt from 'bcrypt';
 import Profile from '../models/profileModel.js'
 import crypto from 'crypto'
 import { json } from 'stream/consumers';
+import PDFDocument from 'pdfkit'
+import fs from 'fs';
+import ConnectionRequest from '../models/connectionModel.js'
+
+
+const convertUserDataToPDF = async (userData) =>{
+    
+    const doc = new PDFDocument();
+
+    const outputPath = crypto.randomBytes(32).toString("hex") + ".pdf";
+    const stream = fs.createWriteStream("uploads/" + outputPath)
+
+    doc.pipe(stream)
+
+    doc.image(`/uploads/${userData.userId.profilePicture}` , {align:" center", width:100});
+    doc.fontSize(14).text(`Name: ${userData.userId.name}`)
+    doc.fontSize(14).text(`Username: ${userData.userId.username}`);
+    doc.fontSize(14).text(`Email: ${userData.userId.email}`);
+    doc.fontSize(14).text(`Bio: ${userData.userId.bio}`)
+    doc.fontSize(14).text(`Current Position ${userData.currentPost}`)
+    doc.fontSize(14).text("Past Work")
+    userData.pastWork.forEach((work, index) =>{
+        doc.fontSize(14).text(`Company Name: ${work.company}`)
+        doc.fontSize(14).text(`Position: ${work.position} `)
+        doc.fontSize(14).text(`years ${work.years}`)
+    })
+
+    doc.end()
+
+    return outputPath;
+
+}
 
 export const register = async (req,res) => {
     try {
@@ -143,5 +175,117 @@ export const getAllUserProfile =  async(req, res)=>{
         return res.json({profiles})
     } catch (error) {
         return res.status(500).json({message: error.message});
+    }
+}
+
+
+export const downloadProfile = async (req, res) =>{
+   try {
+       const user_id = req.query.id;
+       const userProfile = await Profile.findOne({userId: user_id}).populate('userId' , 'name username email profilePicture')
+       let outputPath = await convertUserDataToPDF(userProfile);
+
+       return res.json({message: outputPath})
+   } catch (error) {
+        return res.status(500).json({message: error.message})
+   }
+}
+
+
+export const sendConnectionRequest = async(req,res)=>{
+    const {token, connectionId} = req.body;
+
+    try {
+        const user = await User.findOne({token});
+
+        if(!user){
+            return res.status(404).json({message:"user not found"});
+        }
+
+        const connectionUser = await User.findOne({_id: connectionId});
+
+        if(!connectionUser){
+            return res.status(404).json({message: "connection user not found"});
+        }
+        const existingRequest = await ConnectionRequest.findOne({
+            userId: user._id,
+            connectionId: connectionUser._id
+        })
+        if(existingRequest){
+            return res.status(400).json({message:"request already sent"})
+        }
+
+        const request = new ConnectionRequest({
+            userId: user._id,
+            connectionId: connectionUser._id
+        })
+
+        await request.save();
+        return res.json({message: "request sent"})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
+
+export const getMyConnectionRequests = async (req, res)=>{
+    const {token} = req.body;
+
+    try {
+        const user = await User.findOne({token});
+        if(!user){
+            return res.status(404).json({message:" user not found"})
+        }
+
+        const connections = await ConnectionRequest.find({userId: user._id}).populate('connectionId', 'name username email profilePicture');
+
+        return res.json({connections})
+
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
+
+
+export const whatAreMyConnection = async (req,res) =>{
+    const {token} = req.body;
+    try{
+        const user = await User.findOne({token});
+
+        if(!user){
+            return res.status(404).json({mesage: "user not found"})
+        }
+
+        const connections = await ConnectionRequest.find({connectionId: user._id}).populate('userId', 'name username email profilePicture')
+        return res.json(connections)
+    }catch(error){
+        return res.status(500).json({message: error.message})
+    }
+}
+
+export const acceptConnectionRequest = async (req, res) => {
+    const {token, requestId, action_type} = req.body;
+
+    try {
+        const user = await User.findOne({token});
+        if (!user) {
+            return res.status(404).json({ mesage: "user not found" })
+        }
+
+        const connection = await ConnectionRequest.findOne({_id: requestId})
+
+        if(!connection){
+            return res.status(404).json({message:"connection not found"})
+        }
+
+        if(action_type === "accept"){
+            connection.status_accepted = true;
+        }else{
+            connection.status_accepted = false;
+        }
+
+        await connection.save();
+        return res.json({message: "request updated"})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
     }
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";   
-import { getAllUserProfile, getUserAndProfile, login, register, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/userController.js";
+import { acceptConnectionRequest, downloadProfile, getAllUserProfile, getMyConnectionRequests, getUserAndProfile, login, register, sendConnectionRequest, updateProfileData, updateUserProfile, uploadProfilePicture, whatAreMyConnection } from "../controllers/userController.js";
 import multer from "multer";
 
 const router = Router();
@@ -23,6 +23,11 @@ router.route('/user_update').post(updateUserProfile)
 router.route("/get_user_and_profile").get(getUserAndProfile);
 router.route('/update_profile_data').post(updateProfileData);
 
-router.route("/get_all_user_profile").get(getAllUserProfile);
+router.route("/user/get_all_user_profile").get(getAllUserProfile);
+router.route("/user/download_resume").get(downloadProfile)
+router.route("/user/send_connection_request").post(sendConnectionRequest);
+router.route("/user/getConnectionRequests").get(getMyConnectionRequests);
+router.route("/user/user_connection_request").get(whatAreMyConnection)
+router.route("/user/accept_connection_request").post(acceptConnectionRequest);
 
 export default router;
